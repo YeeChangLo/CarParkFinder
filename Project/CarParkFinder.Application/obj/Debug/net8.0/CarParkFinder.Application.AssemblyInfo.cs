38688d7df3030d64cf1702dd28e0913c32b5c8bd
YeeChangLo <loyeechang06@gmail.com>
2025-03-31 00:19:06 +0800

    - recompiled

--- Project/CarParkFinder.Application/obj/Debug/net8.0/CarParkFinder.Application.AssemblyInfo.cs
+++ Project/CarParkFinder.Application/obj/Debug/net8.0/CarParkFinder.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarParkFinder.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2657483d7a8668f09321e606102e4e96795028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baad5c3e121b74be863c9eca69c99a2bf741abf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarParkFinder.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarParkFinder.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
