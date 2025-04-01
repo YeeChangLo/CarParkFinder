6548dc2452db5d1e5d87504896fd492565017f1e
YeeChangLo <loyeechang06@gmail.com>
2025-04-01 16:38:47 +0800

    - Add delete endpoint

--- Project/CarParkFinder.Tests/obj/Debug/net8.0/CarParkFinder.Tests.AssemblyInfo.cs
+++ Project/CarParkFinder.Tests/obj/Debug/net8.0/CarParkFinder.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarParkFinder.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38688d7df3030d64cf1702dd28e0913c32b5c8bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f794b0e898e27fb66dc5f458f592d7200cc10c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarParkFinder.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarParkFinder.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
